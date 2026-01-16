28feef6836e771f96e24dcee921beb6bd28ea252
Gungun-Singh <144252652+Gungun-Singh@users.noreply.github.com>
2026-01-16 11:54:33 +0530

    Time: 0 ms (100%), Space: 43.9 MB (32.06%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -9,24 +9,23 @@ class Solution {
                 return mid;
             }
 
-            //LEFT HALF IS SORTED
-            if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
+        //LEFT HALF
+            if(nums[left]<= nums[mid]){
+                if(nums[left]<= target && target<nums[mid]){
+                    right = mid-1;
                 }
-            } else {
-                //RIGHT HALF IS SORTED
-                if (target <= nums[right] && target > nums[mid]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
+                else{
+                    left =mid+1;
                 }
-
+            }
+            else{
+                if(nums[right]>= target && target> nums[mid]){
+                    left= mid+1;
+                }else{
+                    right =mid-1;
+                }
             }
         }
         return -1;
-
     }
 }
