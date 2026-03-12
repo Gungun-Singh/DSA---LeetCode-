9f2360126ce7a07660fb7628ddfcc9de793ce7a8
Gungun-Singh <144252652+Gungun-Singh@users.noreply.github.com>
2026-03-12 22:32:55 +0530

    Time: 1 ms (98.55%), Space: 43.4 MB (81.91%) - LeetHub

---
+++ 3340-check-balanced-string/3340-check-balanced-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isBalanced(String num) {
+        int sum=0;
+
+        for(int i=0;i<num.length();i++){
+            if(i%2==0){
+                sum+= num.charAt(i)-'0';
+            }else{
+                sum-= num.charAt(i)-'0';
+            }
+        }
+        if(sum ==0){
+            return true;
+        }
+        return false;
+    }
+}
