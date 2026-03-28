b7cfaa8c4cba3f76bb21825a991e82373f36f890
Gungun-Singh <144252652+Gungun-Singh@users.noreply.github.com>
2026-03-28 21:45:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -0,0 +1,16 @@
+class Solution {
+  
+        public int winner(int n,int k){
+
+        if(n==1) return 0;
+
+          return (winner(n-1,k)+k)%n;
+    }
+      public int findTheWinner(int n, int k) {
+        
+        int ans = winner(n,k);
+        return ans+1;
+
+
+    }
+}
