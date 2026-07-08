f4d5bb9b8f42f8215cdd2168fd78b8245594be95
Gungun-Singh <144252652+Gungun-Singh@users.noreply.github.com>
2026-07-08 21:53:00 +0530

    Time: 238 ms (100%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
--- CREATE TABLE SecondHighestSalary
-SELECT ( SELECT DISTINCT salary
-  FROM Employee
-  ORDER BY salary DESC
-  LIMIT 1 OFFSET 1
-)  AS SecondHighestSalary ;
+-- CREATE TABLE( SecondHighestSalary
+select coalesce((select distinct salary
+from Employee
+order by salary desc
+limit 1 offset 1),null)
+as SecondHighestSalary
