ae9d0826f40156ca48981ed71b50287ec2697efc
Gungun-Singh <144252652+Gungun-Singh@users.noreply.github.com>
2026-08-04 18:56:24 +0530

    Time: 3 ms (81.76%), Space: 46.7 MB (80.06%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -1,19 +1,40 @@
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
+
+        // ArrayList<Integer> list = new ArrayList<>();
+        
+        // if(nums[nums.length-1] - nums[0] +1 == nums.length){
+        //     return list;
+        // }
+
+        // int curr= nums[0];
+
+        // for(int i=0;i< nums.length;curr++, i++){
+        //     if(curr < nums[i]){
+        //         list.add(curr);
+        //         i--;
+        //     }
+        // }
+        // return list;
+
+
+        int min =Integer.MAX_VALUE;
+        int maxs =Integer.MIN_VALUE;
+
+        HashSet<Integer> set = new HashSet<>();
 
         ArrayList<Integer> list = new ArrayList<>();
-        
-        if(nums[nums.length-1] - nums[0] +1 == nums.length){
-            return list;
+
+        for(int i=0;i< nums.length;i++){
+            set.add(nums[i]);
+            min= Math.min(min, nums[i]);
+            maxs = Math.max(maxs, nums[i]);
         }
 
-        int curr= nums[0];
-
-        for(int i=0;i< nums.length;curr++, i++){
-            if(curr < nums[i]){
-                list.add(curr);
-                i--;
+        for(int i=min; i<maxs;i++){
+            if(!set.contains(i)){
+                list.add(i);
             }
         }
         return list;
