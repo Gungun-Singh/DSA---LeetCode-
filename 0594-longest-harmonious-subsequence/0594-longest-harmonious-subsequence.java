fca4eba314c6fd3d069b6a2055b2d971115a6888
Gungun-Singh <144252652+Gungun-Singh@users.noreply.github.com>
2026-08-11 12:16:25 +0530

    Time: 16 ms (90.61%), Space: 48.3 MB (40.39%) - LeetHub

---
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findLHS(int[] nums) {
+        Arrays.sort(nums);
+
+        int j=0;
+        int maxL =0;
+
+        for(int i=0;i<nums.length;i++){
+            while(nums[i]-nums[j]>1){
+                j++;
+            }
+            if(nums[i]-nums[j]==1){
+                maxL = Math.max(maxL, i-j+1);
+            }
+        }
+        return maxL;
+    }
+}
