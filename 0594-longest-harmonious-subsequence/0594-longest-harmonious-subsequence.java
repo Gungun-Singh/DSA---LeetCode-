5e73c20d4cec278ed0ac964639e825c535b3cfb1
Gungun-Singh <144252652+Gungun-Singh@users.noreply.github.com>
2026-08-11 12:41:58 +0530

    Time: 16 ms (90.61%), Space: 48.3 MB (40.39%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
@@ -1,18 +1,39 @@
 class Solution {
     public int findLHS(int[] nums) {
-        Arrays.sort(nums);
 
-        int j=0;
-        int maxL =0;
+    HashMap<Integer, Integer> map =new HashMap<>();
+    for(int num:nums){
+        map.put(num, map.getOrDefault(num,0)+1);
+    }
 
-        for(int i=0;i<nums.length;i++){
-            while(nums[i]-nums[j]>1){
-                j++;
+    int maxL =0;
+
+    for (int num : map.keySet()) {
+
+            if (map.containsKey(num + 1)) {
+
+                int len = map.get(num) + map.get(num + 1);
+
+                maxL = Math.max(maxL, len);
             }
-            if(nums[i]-nums[j]==1){
-                maxL = Math.max(maxL, i-j+1);
-            }
-        }
-        return maxL;
+    }
+    return maxL;
+
+
+
+        // Arrays.sort(nums);
+
+        // int j=0;
+        // int maxL =0;
+
+        // for(int i=0;i<nums.length;i++){
+        //     while(nums[i]-nums[j]>1){
+        //         j++;
+        //     }
+        //     if(nums[i]-nums[j]==1){
+        //         maxL = Math.max(maxL, i-j+1);
+        //     }
+        // }
+        // return maxL;
     }
 }
