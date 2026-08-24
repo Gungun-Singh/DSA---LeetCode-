bec7852e86e4e3a7ef764e1df0d85f27e414be78
Gungun-Singh <144252652+Gungun-Singh@users.noreply.github.com>
2026-08-24 20:06:09 +0530

    Time: 272 ms (85.37%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,11 +1,9 @@
 # Write your MySQL query statement below
--- CREATE TABLE( SecondHighestSalary
--- select coalesce((select distinct salary
--- from Employee
--- order by salary desc
--- limit 1 offset 1),null)
--- as SecondHighestSalary
-
-select max(salary) as SecondHighestSalary 
+select max(salary) as SecondHighestSalary
+from 
+(select distinct salary
 from Employee
-where salary< (select max(salary) from Employee)
+order by salary desc
+limit 1 offset 1)
+as Employee
+;
